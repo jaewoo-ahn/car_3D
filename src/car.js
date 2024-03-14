7547b6390bd1955ab4ea80979fa0c52aed90baad
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-03-14 19:24:13 +0900

    car

--- src/car.js
+++ src/car.js
@@ -1,7 +1,24 @@
+import {useLoader} from "@react-three/fiber";
+import {useEffect} from "react";
+import {GLTFLoader} from "three-stdlib";
+import {Mesh} from "three";
+
 export function Car() {
-  return (
-    <primitive>
-      <></>
-    </primitive>
+  const gltf = useLoader(
+    GLTFLoader,
+    process.env.PUBLIC_URL + "models/car/scene.gltf"
   );
+
+  useEffect(() => {
+    gltf.scene.scale.set(0.005, 0.005, 0.005);
+    gltf.scene.position.set(0, -0.035, 0);
+    gltf.scene.traverse((object) => {
+      if (object instanceof Mesh) {
+        object.castShadow = true;
+        object.receiveShadow = true;
+        object.material.envMapIntensity = 20;
+      }
+    });
+  }, [gltf]);
+  return <primitive object={gltf.scene} />;
 }
